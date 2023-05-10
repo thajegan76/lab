5158fa1d8fd5e67268b4636c734169366f71deca
Thayanithy Jegan <thajegan76@gmail.com>
2023-05-10 15:18:34 +0800

    2 way data binding
    2 way data binding

--- Angular/ecommerce/src/app/app.module.ts
+++ Angular/ecommerce/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PageHeaderComponent } from './page-header/page-header.component';
 import { PageFooterComponent } from './page-footer/page-footer.component';
 import { ProductsComponent } from './products/products.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageHeaderComponent,
     PageFooterComponent,
-    ProductsComponent
+    ProductsComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
