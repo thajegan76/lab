843b202ba1c524f6fb75b8b20e655ff1fde7c105
Thayanithy Jegan <thajegan76@gmail.com>
2023-05-11 12:17:25 +0800

    ProductService is fully done
    ProductService is fully done

--- Angular/ecommerce/src/app/products/products.component.ts
+++ Angular/ecommerce/src/app/products/products.component.ts
@@ -1,26 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { CartItem } from '../models/cart-item';
 import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   
   public colspanValue:number;
   public isGallery:boolean;
+  public products:Product[];
   public searchResults:Product[];
   public shoppingCart:CartItem[];
 
-  public products:Product[] = []
+  public productService:ProductService;
+  public productService$?:Subscription;
 
-  constructor() {
+  constructor(productService:ProductService) {
     this.colspanValue = 2;
     this.isGallery = true;
+    this.products = [];
     this.searchResults = [];
     this.shoppingCart = [];
+    this.productService = productService;
+  }
+
+  ngOnInit(): void {
+    this.productService$ = this.productService.findAll().subscribe((products:Product[]) => {
+      this.products = products;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.productService$?.unsubscribe();
   }
 
   public doChangeView() {
