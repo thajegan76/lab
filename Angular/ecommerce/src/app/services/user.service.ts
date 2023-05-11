ed04e7adbc8bf48b6ea85d4dd274f618a99168b8
Thayanithy Jegan <thajegan76@gmail.com>
2023-05-11 14:39:10 +0800

    User Service
    User Service

--- Angular/ecommerce/src/app/services/user.service.ts
+++ Angular/ecommerce/src/app/services/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private API_ENDPOINT:string = "http://localhost:3000/users";
+  private httpClient:HttpClient;
+
+  constructor(httpClient:HttpClient) { 
+    this.httpClient = httpClient;
+  }
+
+  public findAll():Observable<User[]> {
+    return this.httpClient.get<User[]>(this.API_ENDPOINT);
+  }
+
+  public save(user:User):Observable<User> {
+    return this.httpClient.post<User>(this.API_ENDPOINT, user);
+  }
+
+  public update(user:User):Observable<User> {
+    return this.httpClient.put<User>(this.API_ENDPOINT + "/" + user.id, user);
+  }
+
+  public delete(id:number):Observable<User> {
+    return this.httpClient.delete<User>(this.API_ENDPOINT + "/" + id);
+  }
+
 }
